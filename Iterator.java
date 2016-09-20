7da3d330c2cb452ff22db0be16d0adf443f64b9c
Jeija <norrepli@gmail.com>
2016-09-20 12:43:06 +0200

    Clean up: Remove Lorem Ipsum, sample code and sample boxes

--- Iterator.java
+++ Iterator.java
@@ -2,9 +2,8 @@ public interface Iterator<E> {
 	public boolean hasNext();
 	public E next();
 
-	// Löscht Element, welches zuletzt
-	// von next() zurückgegeben wurde
-	// aus der dahinterliegenden Collection
-	// Nur 1x pro next()-Aufruf
+	// Löscht Element, welches zuletzt von next()
+	// zurückgegeben wurde aus der dahinterliegenden
+	// Collection. Nur 1x pro next()-Aufruf verwendbar.
 	public void remove();
 }
